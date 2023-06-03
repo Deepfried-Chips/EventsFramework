dabd535b2aac4aa9eb64872cc59b99f48373fe7f
Deepfried Chips <chips@dannypoto.com>
2023-06-03 11:04:37 +0300

    remove AssemblyInfo.cs, try using NWApiPermissionSystem

---
+++ EventsFramework/Commands/RunScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommandSystem;
+using NWAPIPermissionSystem;
+
+namespace EventsFramework.Commands
+{
+    public class RunScript : ICommand
+    {
+        public string Command { get; } = "runscript_ef";
+
+        public string[] Aliases { get; } = new string[] { "runscript" };
+
+        public string Description { get; } = "Run an event script";
+
+        public string perm { get; } = "eventsframework.runscript";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!sender.CheckPermission(perm))
+            {
+                response = $"You do not have the right permission to use this command, permission required: {perm}";
+                return false;
+            }
+
+            response = "Not implemented";
+            return true;
+        }
+    }
+}
